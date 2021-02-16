50de43b32cd20270e2cd60675d0db73318b820da
Apostolos Florakis <32836747+apof@users.noreply.github.com>
2021-02-16 15:48:41 +0000

    Add files via upload

---
+++ Project_1/Preprocess.py
@@ -0,0 +1,37 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import  WordNetLemmatizer
+nltk.download('stopwords')
+
+## preprocess the passages tokenising each sentence, convering to lower case, excluding non alphabetic words
+## removing stopwords and stemming
+def process_data(data,rm_stopwords = False):
+
+	stop_words = set(stopwords.words('english'))
+
+	processed_sentences = []
+	for sentence in data:
+
+		## tokenise each sentence
+		tokenised_sentence = sentence.split(" ")
+
+		##convert to lower case
+		sentence = [w.lower() for w in tokenised_sentence]
+
+		##exclude non alphabetic words
+		only_alpha_sentence = [word for word in sentence if word.isalpha()]
+		
+		## remove stop words
+		if(rm_stopwords == True):
+			filtered_sentence = [w for w in only_alpha_sentence if not w in stop_words]
+		else:
+			filtered_sentence = only_alpha_sentence
+
+		lemmatized_sentence = []
+		## stemming
+		lemmatizer = WordNetLemmatizer()
+		for word in filtered_sentence:
+			lemmatized_sentence.append(lemmatizer.lemmatize(word))
+
+		processed_sentences.append(lemmatized_sentence)
+	return processed_sentences
---
+++ Project_1/Utils.py
@@ -0,0 +1,200 @@
+import collections
+import matplotlib.pyplot as plt
+import numpy as np
+import Preprocess
+from sklearn.linear_model import LinearRegression
+
+
+def read_collection(filename):
+	with open(filename) as f:
+		content = f.readlines()
+		# remove whitespace characters like `\n` at the end of each line
+		content = [x.strip() for x in content]
+		return content
+
+def derive_frequencies_from_collection(passage_collection):
+
+	## build a dictionary storing the frequency of each word appeared on the collection
+	word_frequence_dictionary = {}
+
+	total_words = 0
+
+	for passage in  passage_collection:
+		for word in passage:
+			if(word_frequence_dictionary.get(word) == None):
+				word_frequence_dictionary[word] = 1
+			else:
+				word_frequence_dictionary[word] += 1
+
+			total_words += 1
+
+	## sort the dictionaary according in a decreasing frequency order
+	ordered_dictionary = sorted(word_frequence_dictionary.items(), key=lambda kv: kv[1], reverse = True)
+
+	return ordered_dictionary,total_words
+
+def plot_word_frequencies(ordered_dictionary,total_words):
+
+	tuple_list = []
+
+	rank_list = []
+	log_ranked_list = []
+	frequency_list = []
+	log_frequency_list = []
+
+	mult = []
+
+	rank = 1
+	for (key,value) in ordered_dictionary:
+		## store (rank,frequency,proba)
+		tuple_list.append((key,rank,value,float(value/total_words)))
+		frequency_list.append(float(value/total_words))
+		log_frequency_list.append(np.log(float(value)))
+		rank_list.append(rank)
+		log_ranked_list.append(np.log(rank))
+		mult.append(float(value/total_words)*rank)
+		rank += 1
+
+
+	rank_array = np.array(log_ranked_list).reshape(-1,1)
+	rank_frequency = np.array(log_frequency_list).reshape(-1,1)
+
+	### fit a linear regression classifier to the data
+	regressor = LinearRegression().fit(rank_array, rank_frequency)
+
+	print("R^2 coefficient is: " + str(regressor.score(rank_array, rank_frequency)))
+	print("Linear Model Weight: " + str(regressor.coef_[0][0]))
+	print("Linear Model Bias: " + str(regressor.intercept_[0]))
+
+	linear_regression_result = []
+	for i in range(len(log_ranked_list)):
+		linear_regression_result.append(regressor.coef_[0][0]*log_ranked_list[i] + regressor.intercept_[0])
+	
+
+	frequency_plot=plt.plot(rank_list,frequency_list,label="Frequency per Rank")
+	plt.legend()
+	plt.title("Passage Collection Distribution - Zipfs Law")
+	plt.xlabel("rank")
+	plt.ylabel("word frequency")
+	plt.show()
+
+	frequency_plot=plt.plot(log_ranked_list,log_frequency_list,label="Log Frequency per Rank")
+	linearly_fited_plot=plt.plot(log_ranked_list,linear_regression_result,label="Linear Regression Fiting")
+	plt.legend()
+	plt.title("Passage Collection Distribution - Zipfs Law - Logarithmic Scale")
+	plt.xlabel("rank (logarithmic)")
+	plt.ylabel("word frequency (logarithmic)")
+	plt.show()
+
+	print("Mean Value: " + str(np.mean(np.array(mult))))
+	print("Std: " + str(np.std(np.array(mult))))
+
+
+
+def get_dictionaries(df1,df2):
+
+	query_dict = {}
+	passage_dict = {}
+	canidates_dict = {}
+
+	for index, row in df1.iterrows():
+		qid = row['qid']
+		pid = row['pid']
+		query = row['query']
+		passage = row['passage']
+		## create a query index
+		if(query_dict.get(qid) == None):
+			query_dict[qid] = query
+		## create a passage index
+		if(passage_dict.get(pid) == None):
+			passage_dict[pid] = passage
+		## create a query canidate passages index
+		if(canidates_dict.get(qid) == None):
+			canidates_dict[qid] = [pid]
+		else:
+			candidates_list = canidates_dict.get(qid)
+			candidates_list.append(pid)
+			canidates_dict[qid] = candidates_list
+
+	test_query_dict = {}
+	for index, row in df2.iterrows():
+		test_query_dict[row['qid']] = row['query']
+
+	return query_dict, test_query_dict, passage_dict, canidates_dict
+
+## create an inverted index given a query id
+## the data structure is created based on the candidate passages that have already been retrieved for the query
+def inverted_index(query_id,passage_dict,canidates_dict):
+
+	inverted_index_dictionary = {}
+	## get the passages ids
+	canidate_ids = canidates_dict.get(query_id)
+
+	canidate_passage_list = []
+	for passage_id in canidate_ids:
+		canidate_passage_list.append(passage_dict.get(passage_id))
+
+	## preprocess the candidate passages
+	preprocessed_passages = Preprocess.process_data(canidate_passage_list,rm_stopwords = True)
+
+	## create the inverted index
+	for i in range(len(preprocessed_passages)):
+		for token in preprocessed_passages[i]:
+			## if a passage dictionary does not exist for this token
+			if(inverted_index_dictionary.get(token) == None):
+				inside_dict = {}
+				inside_dict[canidate_ids[i]] = 1
+				inverted_index_dictionary[token] = inside_dict
+			## if a passage dictionary already exists for this token update it
+			else:
+				inside_dict = inverted_index_dictionary.get(token)
+				if(inside_dict.get(canidate_ids[i]) == None):
+					inside_dict[canidate_ids[i]] = 1
+				else:
+					inside_dict[canidate_ids[i]] += 1
+
+	candidates_dict = {}
+	for i in range(len(canidate_ids)):
+		candidates_dict[canidate_ids[i]] = preprocessed_passages[i]
+
+
+	return inverted_index_dictionary, candidates_dict
+
+
+## implement TF-IDF vectorisation for a collection of passages - given the inverted index data structure
+def TFIDF_vectorisation(query_id,inverted_index,candidates_dict):
+	
+	ids_list = []
+	vectorised_representations = []
+
+	## create representations for each of the candidate passages
+	for passage_id in candidates_dict:
+		ids_list.append(passage_id)
+		passage = candidates_dict.get(passage_id)
+		passage_representation = []
+		for token in passage:
+
+			## frequency of term in the documnet
+			freq = inverted_index.get(token).get(passage_id)
+			if(freq == None):
+				freq = 0
+			## calculate term frequency
+			tf = freq/len(passage)
+
+			## compute how many passages contain this term
+			term_occur = inverted_index.get(token)
+			if(term_occur == None):
+				term_occur = 0
+			else:
+				term_occur = len(term_occur)
+
+			## calculate inverse document frequency
+			idf = len(candidates_dict)/(term_occur + 1)
+
+			tfidf = tf*idf
+
+			passage_representation.append(tfidf)
+
+		vectorised_representations.append(passage_representation)
+
+	return vectorised_representations, ids_list
