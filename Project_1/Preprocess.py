66bacb78f1a134714c5e2884a602c59c84c2dde7
Apostolos Florakis <32836747+apof@users.noreply.github.com>
2021-02-24 21:01:51 +0000

    Add files via upload

--- Project_1/Preprocess.py
+++ Project_1/Preprocess.py
@@ -1,7 +1,11 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import  WordNetLemmatizer
+from nltk.stem import PorterStemmer 
 nltk.download('stopwords')
+from nltk.tokenize import RegexpTokenizer
+
+
 
 ## preprocess the passages tokenising each sentence, convering to lower case, excluding non alphabetic words
 ## removing stopwords and stemming
@@ -9,17 +13,24 @@ def process_data(data,rm_stopwords = False, lemm = True):
 
 	stop_words = set(stopwords.words('english'))
 
+	tokenizer = RegexpTokenizer(r'\w+')
+
+
 	processed_sentences = []
 	for sentence in data:
 
 		## tokenise each sentence
-		tokenised_sentence = sentence.split(" ")
+		#tokenised_sentence = sentence.split(" ")
+
+		tokenised_sentence = tokenizer.tokenize(sentence)
 
 		##convert to lower case
 		sentence = [w.lower() for w in tokenised_sentence]
 
 		##exclude non alphabetic words
-		only_alpha_sentence = [word for word in sentence if word.isalpha()]
+		#only_alpha_sentence = [word for word in sentence if word.isalpha()]
+
+		only_alpha_sentence = sentence
 		
 		## remove stop words
 		if(rm_stopwords == True):
@@ -31,9 +42,11 @@ def process_data(data,rm_stopwords = False, lemm = True):
 
 		if(lemm == True):
 			## stemming
-			lemmatizer = WordNetLemmatizer()
+			## lemmatizer = WordNetLemmatizer()
+			stemmer = PorterStemmer()
 			for word in filtered_sentence:
-				lemmatized_sentence.append(lemmatizer.lemmatize(word))
+				#lemmatized_sentence.append(lemmatizer.lemmatize(word))
+				lemmatized_sentence.append(stemmer.stem(word))
 		else:
 			lemmatized_sentence = filtered_sentence
 
